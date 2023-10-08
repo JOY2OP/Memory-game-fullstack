b6e2cda5e903940159550dcd920dacecdb615a3d
Joy <77735525+JOY2OP@users.noreply.github.com>
2023-10-08 23:57:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//current problem: Not able to import export functions to use it on UI.
+//current problem: Not able to import export functions to other files to use it on UI.
 // import {incrementAttempts} from './public/logic.mjs'
 const incrementAttempts  = require('./public/logic');
 
