7a74f4afa9291521b172a09390284f80cf175ee0
Joy <77735525+JOY2OP@users.noreply.github.com>
2023-10-16 20:21:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//current problem: Not able to import export functions to use it on UI.
-//Node js doesn't have access to DOM, use vite or something
+//current problem: importing exporting modules
 // import {incrementAttempts} from './public/logic.mjs'
-const incrementAttempts  = require('./public/logic');
+// const incrementAttempts  = require('./public/logic');
+// const gameLogic = require('/public/logic');
 
 
 const port = process.env.PORT || 3000;  
@@ -9,15 +9,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-mongoose.connect(process.env.DB_URL);  
+app.use(express.static('public')) //Middleware to render HTML
+app.use(express.json())  //Middleware to handle JSON request bodies
 
-//HighScore Schema
+const mongoose = require('mongoose');   //Step-1 for mongoose
+const {Schema} = mongoose;              //Step-2 for mongoose
+mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true });   //Step-3 for mongoose
+
+//HighScore Schema, step-4 for mongoose 
 const HsSchema = new Schema({
     highscore: Number,
     attempts: Number,
 });
+
+//Creating a model, step-5 for mongoose 
 const HighScorer = mongoose.model('HighScorer',HsSchema);
 
 const newHighScorer = new HighScorer({
@@ -25,15 +30,20 @@ const newHighScorer = new HighScorer({
     // attempts: incrementAttempts.incrementAttempts(),
     
 })
-
 newHighScorer.save();
 
+//Get MongoDB entries
+app.get('/highscores',async (req,res)=>{
+    const users = await HighScorer.find({}) //
+    // const users = await HighScorer.deleteMany({});
 
-app.use(express.static('public')) //Middleware to render HTML
+     
+     if(!users){res.send("No users")}
+    //  else{res.send("users available")}
+     else{res.json(users)}
+   })
 
-app.get('/highscores',(req,res) => {
-    res.json('this is highscores')
-})
+
 
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);
